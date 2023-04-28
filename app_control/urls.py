0710352b375ec6229ae4036604e1437cb612d778
oskarkos <os-da-ar@hotmail.com>
2023-04-28 01:36:27 -0500

    add override invoice

--- app_control/urls.py
+++ app_control/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from .views import (
     DianResolutionView, InventoryView, ShopView, SummaryView, InvoiceView, PurchaseView, SaleByShopView,
-    InventoryGroupView, SalePerformance, InventoryCSVLoaderView
+    InventoryGroupView, SalePerformance, InventoryCSVLoaderView, UpdateInvoiceView
 )
 
 from rest_framework.routers import DefaultRouter
@@ -19,6 +19,9 @@ router.register('top-selling', SalePerformance, "top-selling")
 router.register('invoice', InvoiceView, "invoice")
 router.register('dian-resolution', DianResolutionView, "dian-resolution")
 
+
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path('update-invoice/<str:invoice_number>/',
+         UpdateInvoiceView.as_view(), name='update-invoice')
 ]
