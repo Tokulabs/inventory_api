6b7bee5eb5d10cf6e4ee43d535e5156ed0982d46
Nicolas Franco <nico@Nicolass-MacBook-Pro.local>
2024-02-27 23:11:43 -0500

    add payment terminal constraint in invoices

--- app_control/views.py
+++ app_control/views.py
@@ -185,7 +185,7 @@ class PaymentTerminalView(ModelViewSet):
 
 class InvoiceView(ModelViewSet):
     queryset = Invoice.objects.select_related(
-        "created_by", "shop", "sale_by").prefetch_related("invoice_items")
+        "created_by", "shop", "sale_by", "payment_terminal").prefetch_related("invoice_items")
     serializer_class = InvoiceSerializer
     permission_classes = (IsAuthenticatedCustom,)
     pagination_class = CustomPagination
