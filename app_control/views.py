695be30e28956c76cb657ac05dc42cc35e214b54
Nicolas Franco <nico@Nicolass-MacBook-Pro.local>
2024-04-04 20:28:20 -0500

    dian resolution put and del

--- app_control/views.py
+++ app_control/views.py
@@ -487,22 +487,47 @@ class InventoryCSVLoaderView(ModelViewSet):
 
 
 class DianResolutionView(ModelViewSet):
+    http_method_names = ('get', 'put', 'delete', 'post')
     queryset = DianResolution.objects.all()
     serializer_class = DianSerializer
     permission_classes = (IsAuthenticatedCustom,)
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         if self.request.method.lower() != 'get':
             return self.queryset
 
         data = self.request.query_params.dict()
+        data.pop("page", None)
+        keyword = data.pop("keyword", None)
+
         results = self.queryset.filter(**data)
+
+        if keyword:
+            search_fields = (
+                "created_by", "document_number", "current_number"
+            )
+            query = get_query(keyword, search_fields)
+            return results.filter(query)
 
         return results
 
     def create(self, request, *args, **kwargs):
         request.data.update({"created_by_id": request.user.id})
         return super().create(request, *args, **kwargs)
+
+    def update(self, request, pk=None):
+        dian_res = DianResolution.objects.filter(pk=pk).first()
+        serializer = self.serializer_class(dian_res, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        dian_res = DianResolution.objects.filter(pk=pk).first()
+        dian_res.delete()
+        return Response({"message": "Dian Resolution deleted successfully"}, status=status.HTTP_200_OK)
 
 
 class ReportExporter(APIView):
