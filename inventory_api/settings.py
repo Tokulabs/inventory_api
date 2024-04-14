831d96d95c916b2ee317137fe33b20c93ad32e30
Nicolas Franco <nico@Nicolass-MacBook-Pro.local>
2024-04-14 00:13:40 -0500

    fix settings

--- inventory_api/settings.py
+++ inventory_api/settings.py
@@ -103,11 +103,9 @@ DATABASES = {
 
 # Render PostgreSQL database (live)
 
-
 DATABASES = {
     'default': dj_database_url.parse(config('DB_EXTERNAL_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
