aac41c34b5a35c7f1b641f6db581e59f1b30b015
Nicolas Franco <nico@ip-192-168-1-15.us-east-2.compute.internal>
2024-05-02 22:23:51 -0500

    fix phone errors

--- db/factories.py
+++ db/factories.py
@@ -9,12 +9,14 @@ from datetime import datetime
 from random import randint, uniform
 
 from django.contrib.auth import get_user_model
+
 User = get_user_model()
 
-from app_control.models import (Provider, Inventory, InventoryGroup, PaymentTerminal, PaymentMethod, DianResolution, Invoice, InvoiceItem, Customer)
+from app_control.models import (Provider, Inventory, InventoryGroup, PaymentTerminal, PaymentMethod, DianResolution,
+                                Invoice, InvoiceItem, Customer)
+
 
 class ProviderFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = Provider
         exclude = ("provider_suffix")
@@ -24,23 +26,22 @@ class ProviderFactory(factory.django.DjangoModelFactory):
     provider_suffix = factory.Faker("company_suffix", locale="es_MX")
     legal_name = factory.LazyAttribute(lambda p: '{} {}'.format(p.name, p.provider_suffix))
     nit = factory.Faker("pyint")
-    phone = factory.LazyAttribute("phone_number")
+    phone = factory.LazyAttribute(lambda _: randint(1000000, 9999999))
     email = factory.Faker("company_email")
     bank_account = factory.Faker("credit_card_number")
     account_type = factory.fuzzy.FuzzyChoice(choices=["Type 1", "Type 2", "Type 3", "Type 4"])
     active = factory.Faker("boolean")
 
-class InventoryGroupFactory(factory.django.DjangoModelFactory):
 
+class InventoryGroupFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = InventoryGroup
-    
+
     created_by = User.objects.get_or_create(id="1")[0]
     name = factory.Faker("word", locale="es_MX")
 
 
 class InventoryFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = Inventory
 
@@ -57,54 +58,54 @@ class InventoryFactory(factory.django.DjangoModelFactory):
     usd_price = factory.LazyAttribute(lambda obj: obj.buying_price / 3900)
     active = factory.Faker("boolean")
 
-class CustomerFactory(factory.django.DjangoModelFactory):
 
+class CustomerFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Customer
 
     created_by = User.objects.get_or_create(id="1")[0]
     document_id = factory.Faker("passport_number")
     name = factory.Faker("company", locale="es_MX")
-    phone = factory.LazyAttribute("phone_number")
+    phone = factory.LazyAttribute(lambda _: randint(1000000, 9999999))
     email = factory.Faker("company_email")
     address = factory.Faker("address")
 
-class PaymentTerminalFactory(factory.django.DjangoModelFactory):
 
+class PaymentTerminalFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = PaymentTerminal
-    
+
     created_by = User.objects.get_or_create(id="1")[0]
     account_code = factory.Faker("bban")
     name = factory.Faker("word", locale="es_MX")
     is_wireless = factory.Faker("boolean")
     active = factory.Faker("boolean")
 
-class DianResolutionFactory(factory.django.DjangoModelFactory):
 
+class DianResolutionFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = DianResolution
-        exclude = ("start_date","end_date")
-    
+        exclude = ("start_date", "end_date")
+
     created_by = User.objects.get_or_create(id="1")[0]
     document_number = factory.LazyAttribute(lambda _: randint(8000000000000, 9999999999999))
-    
+
     start_date = datetime(2024, 1, 1)
     end_date = datetime.now()
     from_date = factory.Faker("date_between", start_date=start_date, end_date=end_date)
- 
+
     to_date = factory.Faker("date_this_year", after_today=True)
- 
+
     from_number = factory.LazyAttribute(lambda _: randint(100000, 900000))
     to_number = factory.LazyAttribute(lambda obj: obj.from_number + 100000)
     current_number = factory.LazyAttribute(lambda obj: randint(obj.from_number, obj.to_number))
     active = factory.Faker("boolean")
 
-class InvoiceFactory(factory.django.DjangoModelFactory):
 
+class InvoiceFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Invoice
-    
+
     created_by = User.objects.get_or_create(id="1")[0]
     payment_terminal = factory.SubFactory(PaymentTerminalFactory)
     customer = factory.SubFactory(CustomerFactory)
@@ -116,22 +117,21 @@ class InvoiceFactory(factory.django.DjangoModelFactory):
 
 
 class PaymentMethodFactory(factory.django.DjangoModelFactory):
-
     class Meta:
         model = PaymentMethod
-    
+
     invoice = factory.SubFactory(InvoiceFactory)
     name = factory.fuzzy.FuzzyChoice(choices=["Cash", "Credit Card", "Debit Card", "ACH"])
     paid_amount = factory.LazyAttribute(lambda _: random.uniform(1000, 1000000))
     back_amount = factory.LazyAttribute(lambda obj: obj.paid_amount * 0.2 * random.uniform(0, 1))
     received_amount = factory.LazyAttribute(lambda obj: obj.paid_amount - obj.back_amount)
     transaction_code = factory.Faker("bban")
-    
-class InvoiceItemFactory(factory.django.DjangoModelFactory):
 
+
+class InvoiceItemFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = InvoiceItem
-    
+
     invoice = factory.SubFactory(InvoiceFactory)
     item = factory.SubFactory(InventoryFactory)
     item_name = factory.Faker("word", locale="es_MX")
@@ -142,4 +142,4 @@ class InvoiceItemFactory(factory.django.DjangoModelFactory):
     discount = factory.LazyAttribute(lambda _: Decimal(random.uniform(0.00, 100.00)))
     amount = factory.LazyAttribute(lambda obj: (obj.original_amount * obj.discount) / 100)
     usd_amount = factory.LazyAttribute(lambda obj: (obj.original_usd_amount * obj.discount) / 100)
-    is_gift = is_override = factory.Faker("boolean")
+    is_gift = factory.Faker("boolean")
