1ee0e447f8e078985e2187c1755a046abf4cd4dd
oskarkos <os-da-ar@hotmail.com>
2024-05-09 19:32:51 -0500

    fix users filter

--- user_control/models.py
+++ user_control/models.py
@@ -35,7 +35,7 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     updated_at = models.DateTimeField(auto_now=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True, null=False)
     last_login = models.DateTimeField(null=True)
 
     USERNAME_FIELD = "email"
